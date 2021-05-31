8b16e48a0053af0d4f19f4c47610b90870ac71fc
Lam Nguyen <lam.nguyen.khmt2017@hcmut.edu.vn>
2021-05-31 16:13:47 +0700

    Push code to github successful

--- server/utils/validate.js
+++ server/utils/validate.js
@@ -1,18 +1,18 @@
 function validateCtoK(temperature) {
-    if (temperature < -273.15) {
-        return false;
-    }
-    return true;
+  if (temperature < -273.15) {
+    return false;
+  }
+  return true;
 }
 
 function validateFtoK(temperature) {
-    if (temperature < -459.67) {
-        return false;
-    }
-    return true;
+  if (temperature < -459.67) {
+    return false;
+  }
+  return true;
 }
 
 module.exports = {
-    validateCtoK: validateCtoK,
-    validateFtoK: validateFtoK,
-}
+  validateCtoK: validateCtoK,
+  validateFtoK: validateFtoK,
+};
